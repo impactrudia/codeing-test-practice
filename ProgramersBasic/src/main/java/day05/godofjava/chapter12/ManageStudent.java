9fb98ec1074ff0884fbc685539531c4d5ebb26ea
impactrudia <impactyoung@gmail.com>
2022-04-24 18:56:10 +0900

    feat : 12장 풀기

---
+++ ProgramersBasic/src/main/java/day05/godofjava/chapter12/ManageStudent.java
@@ -0,0 +1,20 @@
+package day05.godofjava.chapter12;
+
+public class ManageStudent {
+
+    public String checkEquals() {
+        Student a = new Student("Min", "Seoul", "010XXXXXXXX", "ask@godofjava.com");
+        Student b = new Student("Min", "Seoul", "010XXXXXXXX", "ask@godofjava.com");
+
+        if (a.equals(b)) {
+            return "Equal";
+        } else {
+            return "Not Equal";
+        }
+    }
+
+    public static void main(String[] args) {
+        ManageStudent manageStudent = new ManageStudent();
+        System.out.println(manageStudent.checkEquals());
+    }
+}
